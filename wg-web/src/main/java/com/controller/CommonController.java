62f00a4c440487e9cd685b48b7e79149b1b5759f
user815886724 <815886724@qq.com>
2019-05-31 13:49:57 +0800

    增加转发方法

--- wg-web/src/main/java/com/controller/CommonController.java
+++ wg-web/src/main/java/com/controller/CommonController.java
@@ -3,6 +3,10 @@ package com.controller;
 
 import com.model.SysModularParameterEntity;
 import com.service.CommonService;
+import common.CallbackResult;
+import common.HttpUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,12 +28,21 @@ public class CommonController {
     @Autowired
     private CommonService service;
 
+    private static Logger logger = LoggerFactory.getLogger(CommonService.class);
+
     @RequestMapping("/api/{handleAction}/{module}/{func}")
     @ResponseBody
-    public String forwardApi(@PathVariable("handleAction") String handleAction,@PathVariable("module")String module,
-                             @PathVariable("func") String func, HttpServletRequest request){
+    public String forwardApi(@PathVariable("handleAction") String handleAction, @PathVariable("module")String module,
+                                     @PathVariable("func") String func, HttpServletRequest request)throws Exception{
         Map<String,Object> param = request.getParameterMap();
-        SysModularParameterEntity paramEntity= service.getParamEntity("test");
-        return null;
+        SysModularParameterEntity paramEntity= service.getParamEntity(handleAction);
+        //先去验证
+        try{
+            String result = HttpUtils.doPost(paramEntity.getUrl(),param);
+            return result;
+        }catch (Exception e){
+            logger.info(e.getMessage());
+            throw new  Exception("请求\""+ handleAction + "\"模块异常");
+        }
     }
 }
